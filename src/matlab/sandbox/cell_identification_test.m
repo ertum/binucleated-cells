17bf0fe4922cfed02eb9b861219faaf1411593e4
andrew.tausz <andrew.tausz@f7de3ee4-a821-79ee-dc43-63019de1f6d4>
2011-06-28 21:16:07 +0000

    Updated cell segmentation test.

---
+++ src/matlab/sandbox/cell_identification_test.m
@@ -0,0 +1,46 @@
+clc; clear; close all;
+
+gaussianFilter = fspecial('gaussian', [10, 10], 10);
+
+initial_image = imread('../../../data/initial_examples/Best quality images/01 cell membrane.tif');
+equalized_image = histeq(initial_image);
+
+black_and_white = im2bw(equalized_image, graythresh(equalized_image));
+
+% perform blurring to smooth image
+blurred = imfilter(black_and_white, gaussianFilter, 'symmetric', 'conv');
+
+inverted_image = 1 - blurred; 
+
+% flood fill holes
+filled_image = imfill(inverted_image,'holes');
+filled_image = imopen(filled_image, ones(5,5));
+
+% remove small connected components fewer than pixels
+min_num_pixels = 100;
+cleaned_image = bwareaopen(filled_image, min_num_pixels);
+
+% obtain perimeters
+perimeters = bwperim(cleaned_image);
+
+% overlay perimeters on original image
+overlayed = imoverlay(initial_image, perimeters, [1, 0, 0]);
+
+subplot(2, 2, 1);
+imshow(initial_image);
+title('initial image');
+
+subplot(2, 2, 2);
+imshow(black_and_white);
+title('thresholded');
+
+subplot(2, 2, 3);
+imshow(blurred);
+title('blurred');
+
+subplot(2, 2, 4);
+imshow(filled_image);
+title('cleaned and inverted');
+
+figure;
+imshow(overlayed);
