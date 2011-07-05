f5fcfe70b349c72e502b9899087712f747aeb3e6
andrew.tausz <andrew.tausz@f7de3ee4-a821-79ee-dc43-63019de1f6d4>
2011-07-05 18:35:08 +0000

    Small update from a while ago.

--- src/matlab/sandbox/cell_identification_test.m
+++ src/matlab/sandbox/cell_identification_test.m
@@ -2,6 +2,8 @@ clc; clear; close all;
 
 gaussianFilter = fspecial('gaussian', [10, 10], 10);
 
+dna_image = imread('../../../data/initial_examples/Best quality images/01 membrane and DNA, annotated.tif');
+
 initial_image = imread('../../../data/initial_examples/Best quality images/01 cell membrane.tif');
 blurred_initial = imfilter(initial_image, gaussianFilter, 'symmetric', 'conv');
 
@@ -16,7 +18,8 @@ inverted_image = 1 - blurred;
 
 % flood fill holes
 filled_image = imfill(inverted_image,'holes');
-filled_image = imopen(filled_image, ones(5,5));
+
+filled_image = imopen(filled_image, strel('disk', 6));
 
 % remove small connected components fewer than pixels
 min_num_pixels = 900;
@@ -26,7 +29,12 @@ cleaned_image = bwareaopen(filled_image, min_num_pixels);
 perimeters = bwperim(cleaned_image);
 
 % overlay perimeters on original image
-overlayed = imoverlay(initial_image, perimeters, [1, 0, 0]);
+overlayed = imoverlay(dna_image, perimeters, [1, 1, 1]);
+
+connected_components = bwconncomp(filled_image);
+labels = labelmatrix(connected_components);
+
+rgb_labels = label2rgb(labels);
 
 subplot(2, 2, 1);
 imshow(initial_image);
@@ -41,7 +49,7 @@ imshow(blurred);
 title('blurred');
 
 subplot(2, 2, 4);
-imshow(filled_image);
+imshow(rgb_labels);
 title('cleaned and inverted');
 
 figure;
